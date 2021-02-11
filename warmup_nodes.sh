0de1620738529bbd5f2928bd299fbe3304a41ee4
Evgeny Blokhin <eb@tilde.pro>
2021-02-11 16:32:34 +0100

    Add machine preparation script

---
+++ warmup_nodes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MACHINEFILE=$(dirname "$0")/nodes
+MACHINES=($( cat $MACHINEFILE ))
+SUDOER=admin # used e.g. at Google Cloud
+KEY_TOCOPY=~/.ssh/id_rsa.pub
+
+for (( i=0; i<${#MACHINES[@]}; i++ )); do
+    ssh -T -o UserKnownHostsFile=~/.ssh/known_hosts -o StrictHostKeyChecking=no ${MACHINES[i]} "whoami"
+    ssh -T ${MACHINES[i]/root/$SUDOER} "sudo -- sh -c 'sed -i s/^.*ssh-rsa/ssh-rsa/ /root/.ssh/authorized_keys'"
+    ssh-copy-id -f -i $KEY_TOCOPY "${MACHINES[i]}"
+done
