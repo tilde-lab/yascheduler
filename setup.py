26a85f8d490b015328ccf6f2bda4fac5d3d35f36
Evgeny Blokhin <eb@tilde.pro>
2022-04-10 22:12:00 +0200

    Require Py>=3.7 (due to dataclasses)

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
             ],
         },
         cmdclass={'install': CustomInstall},
-        python_requires='>=3.5',
+        python_requires='>=3.7',
         classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Science/Research',
@@ -77,8 +77,6 @@ if __name__ == '__main__':
             'Topic :: Software Development :: Libraries :: Python Modules',
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9'
