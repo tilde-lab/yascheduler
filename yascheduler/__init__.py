f47597412dc1ee02889f138f6b2f7e69d6460fb2
Sergei Korolev <knopki@duck.com>
2023-07-26 16:12:25 +0300

    build(package): migrate to pyproject.toml

--- yascheduler/__init__.py
+++ yascheduler/__init__.py
@@ -1,7 +1,7 @@
-from .__version__ import __version__
 from .client import Yascheduler
 from .variables import CONFIG_FILE, LOG_FILE, PID_FILE
 
+__version__ = "1.0.11"
 __all__ = [
     "CONFIG_FILE",
     "LOG_FILE",
