e2ff1b42e551c06f0a6b352b1b119d0fd6de67fa
Sergei Korolev <knopki@duck.com>
2025-04-04 20:42:18 +0300

    build: Update minimum Python version to 3.9

--- yascheduler/__init__.py
+++ yascheduler/__init__.py
@@ -1,14 +1,9 @@
-import sys
+import importlib.metadata
 
 from .client import Yascheduler
 from .variables import CONFIG_FILE, LOG_FILE, PID_FILE
 
-if sys.version_info < (3, 8):
-    import importlib_metadata
-else:
-    import importlib.metadata as importlib_metadata
-
-__version__ = importlib_metadata.version("yascheduler")
+__version__ = importlib.metadata.version("yascheduler")
 __all__ = [
     "CONFIG_FILE",
     "LOG_FILE",
